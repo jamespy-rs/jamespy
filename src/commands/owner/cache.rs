ceec79f5623f8f62c8c372e61ec0b3fb89188cee
jamesbt365 <jamesbt365@gmail.com>
2023-09-07 01:07:24 +0100

    h

--- src/commands/owner/cache.rs
+++ src/commands/owner/cache.rs
@@ -1,5 +1,5 @@
 use poise::serenity_prelude as serenity;
-use ::serenity::{all::{ChannelType, GuildId}, builder::CreateEmbedFooter};
+use poise::serenity_prelude::{ChannelType, CreateEmbedFooter};
 
 use crate::{Context, Error};
 
